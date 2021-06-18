ab31ec8fa371ab582d10791006f1d55fb9cd7d55
José Caldas <jrcaldas@yahoo.com.br>
2021-06-18 11:07:37 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7941d417b14f1dcd568234655b5633b0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a8bfc243-9079-43c5-a4bd-4606ef987ba3'
+};
