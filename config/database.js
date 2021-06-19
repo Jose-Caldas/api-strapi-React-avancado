f8092c49fcecc08a0b42e4ba95da51f876a64043
José Caldas <jrcaldas@yahoo.com.br>
2021-06-19 12:45:45 -0300

    update: database

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'postgres'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '4280'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "dbstrapi"),
+        username: env("DATABASE_USERNAME", "db_user"),
+        password: env("DATABASE_PASSWORD", "password"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
-      options: {}
+      options: {},
     },
   },
 });
